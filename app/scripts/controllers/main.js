3450077afbe12f794866622fc97ed52ca4bddfff
Blake Winton <bwinton@latte.ca>
2013-09-15 14:08:15 -0400

    Cleanup, and get it working not in the root.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,5 @@
 
 angular.module('fezifyApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
     $scope.plaintext = '';
   });
