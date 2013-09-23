a88d49b8860571dfe9752ccec8450a594dae01de
Blake Winton <bwinton@latte.ca>
2013-09-22 21:12:41 -0400

    Change from a filter to a directive, cause if you're injecting html in your filter, you may be doing it wrong.  ;) Also, yo is pretty cool, yo.

---
+++ app/scripts/directives/fezish.js
@@ -0,0 +1,63 @@
+'use strict';
+
+angular.module('fezifyApp')
+  .directive('fezish', function () {
+
+    // corner, wide, u, snake, b, cross
+    // 1, 2, 3, 4
+    var glyphmap = {
+      'a': 'glyph-wide-1',
+      'b': 'glyph-b-4',
+      'c': 'glyph-u-2',
+      'd': 'glyph-corner-4',
+      'e': 'glyph-snake-1',
+      'f': 'glyph-cross-4',
+      'g': 'glyph-wide-2',
+      'h': 'glyph-b-3',
+      'i': 'glyph-u-1',
+      'j': 'glyph-corner-3',
+      'k': 'glyph-snake-4',
+      'l': 'glyph-cross-3',
+      'm': 'glyph-wide-3',
+      'n': 'glyph-b-2',
+      'o': 'glyph-u-4',
+      'p': 'glyph-corner-2',
+      'q': 'glyph-snake-2',
+      'r': 'glyph-cross-2',
+      's': 'glyph-wide-4',
+      't': 'glyph-b-1',
+      'u': 'glyph-u-3',
+      'v': 'glyph-u-3',
+      'w': 'glyph-corner-1',
+      'x': 'glyph-snake-2',
+      'y': 'glyph-cross-1',
+      'z': 'glyph-snake-3',
+      ' ': 'glyph-blank',
+    };
+
+    var getClass = function (a) {
+      return glyphmap[a.toLowerCase()];
+    };
+
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      require: 'ngModel',
+      link: function postLink($scope, $element, $attrs, ngModel) {
+        $element.addClass($attrs['class']);
+        ngModel.$render = function () {
+          var value = ngModel.$viewValue || '';
+          var out = '';
+          for (var i = 0; i < value.length; i++) {
+            var currentChar = value.charAt(i);
+            if (currentChar === '\n') {
+              out += '<br>';
+            } else if (currentChar in glyphmap) {
+              out += '<span class="' + getClass(value.charAt(i)) + '"></span>';
+            }
+          }
+          $element.html(out);
+        };
+      }
+    };
+  });
